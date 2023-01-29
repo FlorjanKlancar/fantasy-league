752e8a8fb6150b9fa4114e2c9eb27b713e18327d
FlorjanKlancar <klancar.florjan99@gmail.com>
2023-01-29 21:41:53 +0100

    tenis changes

---
+++ src/components/tournament/tenis/SelectPlayers.tsx
@@ -0,0 +1,175 @@
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+type Props = {
+  submitHandler: (e: any, player: any) => void;
+};
+
+export default function SelectPlayers({ submitHandler }: Props) {
+  const people = [
+    {
+      id: 1,
+      name: "Wade Cooper",
+      avatar:
+        "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 2,
+      name: "Arlene Mccoy",
+      avatar:
+        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 3,
+      name: "Devon Webb",
+      avatar:
+        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+    },
+    {
+      id: 4,
+      name: "Tom Cook",
+      avatar:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 5,
+      name: "Tanya Fox",
+      avatar:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 6,
+      name: "Hellen Schmidt",
+      avatar:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 7,
+      name: "Caroline Schultz",
+      avatar:
+        "https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 8,
+      name: "Mason Heaney",
+      avatar:
+        "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 9,
+      name: "Claudie Smitham",
+      avatar:
+        "https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      id: 10,
+      name: "Emil Schaefer",
+      avatar:
+        "https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  ];
+
+  const [selected, setSelected] = useState<[{ id: number; name: string }] | []>(
+    []
+  );
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) =>
+          person.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  function classNames(...classes: any) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  return (
+    <form
+      className="flex space-x-3"
+      onSubmit={(e) => submitHandler(e, selected)}
+    >
+      <Combobox value={selected} onChange={setSelected}>
+        <div className="relative mt-1 w-full">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(item: any) => item.name}
+              onChange={(event) => setQuery(event.target.value)}
+              placeholder="Search"
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredPeople.length === 0 && query !== "" ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredPeople.map((person) => (
+                  <Combobox.Option
+                    key={person.id}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? "bg-teal-600 text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <img
+                            src={person.avatar}
+                            alt=""
+                            className="h-6 w-6 flex-shrink-0 rounded-full"
+                          />
+                          <span
+                            className={classNames(
+                              selected ? "font-semibold" : "font-normal",
+                              "ml-3 block truncate"
+                            )}
+                          >
+                            {person.name}
+                          </span>
+                        </div>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+      <button className="btn">1</button>
+      <button className="btn">Select this player</button>
+    </form>
+  );
+}
--- src/components/tournament/tenis/TenisTournamentFormat.tsx
+++ src/components/tournament/tenis/TenisTournamentFormat.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import autoAnimate from "@formkit/auto-animate";
+import React, { useEffect, useRef, useState } from "react";
 import Brackets from "./Brackets";
-import Example from "./PlayersMultiselect";
+import SelectPlayers from "./SelectPlayers";
 
 type Props = {
   setSubmitData?: (data: unknown) => void;
@@ -9,10 +10,47 @@ type Props = {
 };
 
 function TenisTournamentFormat({ setSubmitData, tournamentId, userId }: Props) {
+  const [selectedList, setSelectedList] = useState<any>([]);
+  const parent = useRef(null);
+
+  const submitHandler = (e: any, selected: any) => {
+    e.preventDefault();
+
+    setSelectedList([...selectedList, selected]);
+    console.log("submit", selected);
+  };
+
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current);
+  }, [parent]);
+
+  console.log({ selectedList });
+
   return (
     <div>
       Tenis
-      <Brackets />
+      {/*  <Brackets /> */}
+      <div className="grid grid-cols-2 gap-5">
+        <SelectPlayers submitHandler={submitHandler} />
+        <div>
+          Selected list
+          <div ref={parent}>
+            {selectedList &&
+              selectedList.map((player: any) => (
+                <div className="flex items-center">
+                  <img
+                    src={player.avatar}
+                    alt=""
+                    className="h-6 w-6 flex-shrink-0 rounded-full"
+                  />
+                  <span className={"ml-3 block truncate font-semibold"}>
+                    {player.name}
+                  </span>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
